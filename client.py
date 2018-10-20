dcdd7a62d2c460f6cc3f254302c170c680fc263d
A Kramer <boglinballet@yahoo.com>
2018-10-20 12:57:47 +0200

    add request methods

--- client.py
+++ client.py
@@ -12,6 +12,7 @@ def health_check():
 
 
 def register(name, password, repeat_password):
+<<<<<<< Updated upstream
     # create request body
     body = RegisterRequest(name, password, repeat_password)
 
@@ -19,6 +20,89 @@ def register(name, password, repeat_password):
     response = requests.post(host+register_path)
 
 
+=======
+    path = "/api/register"
+    response = requests.post(host+path)
+    # something
+    pass
+
+def login(email, password):
+    path = "/api/login"
+    response = requests.post(host+path)
+    # something
+    pass
+
+def alpha_register(email, password, repeat_password, alpha_key):
+    path = "/api/alpharegister"
+    response = requests.post(host+path)
+    # something
+    pass
+
+def profile(business_ID, company, first_name, last_name):
+    path = "/api/user/profile"
+    response = requests.put(host+path)
+    # something
+    pass
+
+def fetch_data():
+    path = "/api/user/profile"
+    response = requests.get(host+path)
+    # something
+    pass
+
+def change_password(new_password, old_password, repeat_password):
+    path = "/api/user/changepw"
+    response = requests.put(host+path)
+    # something
+    pass
+
+def feedback(message, reply):
+    path = "/api/user/feedback"
+    response = requests.post(host+path)
+    # something
+    pass
+
+def connect_bank_account(bank_code, extra_secret, save_secret, secret, username):
+    path = "/api/user/connector/bank/account"
+    response = requests.post(host+path)
+    # something
+    pass
+
+def get_all_bank_accounts():
+    path = "/api/user/connector/bank/account"
+    response = requests.get(host+path)
+    # something
+    pass
+
+def get_forecast():
+    path = "/api/user/intelligence/forecast"
+    response = requests.get(host+path)
+    # something
+    pass
+
+def get_partners():
+    path = "/api/user/intelligence/procentpartner/{span}"
+    response = requests.get(host+path)
+    # something
+    pass
+
+def get_top_customers_suppliers():
+    path = "/api/user/intelligence/toppartner/{request_type}/{span}"
+    response = requests.get(host+path)
+    # something
+    pass
+
+def get_smartalerts():
+    path = "api/user/intelligence/smartalerts"
+    response = requests.get(host+path)
+    # something
+    pass
+
+def branches():
+    path = "api/ressources/branches"
+    response = requests.get(host+path)
+    # something
+>>>>>>> Stashed changes
     pass
 
 
